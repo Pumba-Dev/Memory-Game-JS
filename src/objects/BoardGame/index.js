a4c8f4cfaf455ebde4405c133f299ebb67ff0f73
PumbaSD <dudu32769144@gmail.com>
2021-11-08 18:15:40 -0300

    Create CardFrontBack

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,9 +1,9 @@
 import "./style.css"
-import CardGame from "./../../components/CardGame"
+import CardFrontBack from "./../../components/CardFrontBack"
 
 function BoardGame(NumOfCards) {
-    const $cardGame = CardGame();
-    const $CardGroup = $cardGame.repeat(NumOfCards);
+    const $cardFrontBack = CardFrontBack();
+    const $CardGroup = $cardFrontBack.repeat(NumOfCards);
     
     return `
         <section class="board-game">
