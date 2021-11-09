b7c9ad52b5a27851c8e73569ab6a13914aaefc81
PumbaSD <dudu32769144@gmail.com>
2021-11-09 13:42:34 -0300

    Card Flip CSS Animation

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,16 @@
 import CardGame from "./../CardGame"
+import "./style.css"
 
 function CardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("javascript")}
+            <div class="front">
+                ${CardGame()}
+            </div>    
+            <div class="back">
+                ${CardGame("javascript", "JavaScript Pixel Logo")}
+            </div>
+            
         </article>
     `;
 }
