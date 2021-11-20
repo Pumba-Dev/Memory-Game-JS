49fb8c378289e4e8c03c50b270276e129780f431
PumbaSD <dudu32769144@gmail.com>
2021-11-20 14:56:33 -0300

    Fix Bug of Three Card Selected

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,18 +2,10 @@ import CardGame from "./../CardGame"
 import "./style.css"
 
 function CardFrontBack(icon) {
-    window.cardFrontBack = {};
-    
-    window.cardFrontBack.handleClick_CardFlip = (event) => { 
-        const $origin = event.target;
-        const $cardFrontBack = $origin.closest('.card-front-back');
-        $cardFrontBack.classList.add("-active");
-    }
-
+   
     return /*html*/`
         <article 
             class="card-front-back" 
-            onClick="cardFrontBack.handleClick_CardFlip(event)"
         >
             <div class="front">
                 ${CardGame()}
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -2,12 +2,36 @@ import "./style.css"
 import CardFrontBack from "../../components/CardFrontBack"
 import cards from "./data.js"
 
-function BoardGame(NumOfCards) {
+function BoardGame() {
+
+    function flipCard(event){
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest(".card-front-back");
+        $cardFrontBack.classList.add("-active");
+    }
+
+    function addFlipEvent() {
+        const $boardGame = document.querySelector(".board-game");
+        const $cardsGame = $boardGame.querySelectorAll(".card-front-back");
+        $cardsGame.forEach((card) => {
+            card.addEventListener("click", flipCard);
+        });
+    }
+    window.onload = addFlipEvent;
+
     window.boardGame = {};
-    window.boardGame.handleClickTwoCards = () => {
+    window.boardGame.handleClick = () => {
         const $boardGame = document.querySelector(".board-game");
         const $cardsActive = $boardGame.querySelectorAll(".card-front-back.-active");
         const $arrowDown = document.querySelector(".arrow-down");
+
+        const disableFlip = () => {
+            const $cards = $boardGame.querySelectorAll(".card-front-back");
+            $cards.forEach((card) => {
+                card.removeEventListener("click", flipCard);
+            });
+        };
+
         const hideCards = () => {
             $cardsActive.forEach((card) => {
                 card.classList.remove("-active");
@@ -20,21 +44,23 @@ function BoardGame(NumOfCards) {
         };
         
         if($cardsActive.length == 2) {
+            disableFlip();
             setTimeout(() => {
                 hideCards();
                 swapPlayers();
+                addFlipEvent();
             }, 1000);
         }
-    }
+    }   
 
-    const htmlCardList = cards.map((card) => 
-        CardFrontBack(card.toLowerCase())
-    );
-    // console.log(htmlCardList);
+    const htmlCardList = cards.map((card) => CardFrontBack(card)); 
     const $htmlCards = htmlCardList.join('');
-    
+
     return /*html*/`
-        <section class="board-game" onClick="boardGame.handleClickTwoCards()">
+        <section 
+            class="board-game" 
+            onClick="boardGame.handleClick()"
+            >
             ${$htmlCards}
         </section>
     `;
